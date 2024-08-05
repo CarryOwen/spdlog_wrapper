db7b596941be73963d7f1da6fdc8c325487fc2aa
xzzoyw <yiwen.ou@unix-group.ai>
2024-08-05 11:30:37 +0800

    first commit

---
+++ build/CMakeFiles/app_log.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app_log.dir/main.cpp.o"
+  "app_log"
+  "app_log.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app_log.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/log.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/log.dir/spdlog.cpp.o"
+  "liblog.pdb"
+  "liblog.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/log.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
