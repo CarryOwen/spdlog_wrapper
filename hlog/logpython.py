10a720b93d957dc0624aa2e103ed0b279bf5703f
xzzoyw1314 <xzzoyw1314@gmail.com>
2024-08-27 10:13:49 +0800

    增加了python格式化输出功能

--- hlog/logpython.py
+++ hlog/logpython.py
@@ -2,7 +2,7 @@ import sys
 # 生成的动态库路径
 sys.path.append('hlog/build')
 import log_wrapper
-
+import inspect
 
 logger = log_wrapper.LogWrapper()
 
@@ -11,6 +11,7 @@ logger.AddColorConsole()
 logger.AddRotatingFile("hardBridge", "log/hardBridge")
 
 logger.Init()
-
+cnt=1
 logger.log_debug("This is a debug message")
 logger.log_info("This is a info message")
+logger.log_debug("test: {} {} {}", inspect.currentframe().f_code.co_filename,inspect.currentframe().f_lineno,cnt) 
